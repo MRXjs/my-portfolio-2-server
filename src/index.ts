c07b2a0b7ee4e584b8e621278e7a6a2c2b3f7d9a
MRX <sureshmadawa763@gmail.com>
2024-08-26 20:48:14 +0530

    origin change

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json({ limit: '50mb' }));
 // cors => cross origin resource sharing
 app.use(
     cors({
-        origin: ['http://localhost:3000', 'https://mahumacademy.lk'],
+        origin: ['http://localhost:3000', 'https://suresranathunga.com'],
         credentials: true,
     }),
 );
